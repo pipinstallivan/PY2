f6cdeb791a8e4ee6c1489097755437a2c8b6a581
pipinstallivan <115542587+pipinstallivan@users.noreply.github.com>
2023-01-29 17:29:51 +0300

    Лабораторная работа 3
    1. Свойства для переменных author и name заданы таким образом, вне класса их нельзя изменять
    2. При инициализации классов Paperbook и Audiobook проверяется корректность типов специфических атрибутов
    3. При попытке изменить аттрибуты duration и pages тоже проверяется корректность типов
    4. Для Класса Audiobook перегружен метод __str__ (книга ->аудиокнига)

--- Лабораторная 3/main.py
+++ Лабораторная 3/main.py
@@ -1,31 +1,70 @@
+def correct_insert(new_value, std_type):
+    if isinstance(new_value, std_type):
+        return new_value
+    else:
+        raise ValueError(f"ожидается значение типа {std_type}")
+
 class Book:
     """ Базовый класс книги. """
+
     def __init__(self, name: str, author: str):
-        self.name = name
-        self.author = author
+        self.__name = name
+        self.__author = author
 
     def __str__(self):
         return f"Книга {self.name}. Автор {self.author}"
 
     def __repr__(self):
-        return f"{self.__class__.__name__}(name={self.name!r}, author={self.author!r})"
+        return f"{self.__class__.__name__}(name={self.__name!r}, author={self.__author!r})"
+    def set_cant(self, new_name: str):
+        pass
+    def get_name(self):
+        return self.__name
+    def get_author(self):
+        return self.__author
+    name = property(get_name, set_cant)
+    author = property(get_author, set_cant)
 
-
-class PaperBook:
+class PaperBook(Book):
     def __init__(self, name: str, author: str, pages: int):
-        self.name = name
-        self.author = author
-        self.pages = pages
+        self._Book__name = name
+        self._Book__author = author
+        self.__pages = correct_insert(pages, int)
 
-    def __str__(self):
-        return f"Книга {self.name}. Автор {self.author}"
+    def get_pages(self):
+        return self.__pages
+
+    def set_pages(self, new_size: int):
+        if isinstance(new_size, int):
+            self.__pages = new_size
+        else:
+            raise ValueError("Поврежденные книги не обрабатываем,"
+                             "вводите целое число страниц")
+    pages = property(get_pages, set_pages)
 
 
-class AudioBook:
+class AudioBook(Book):
     def __init__(self, name: str, author: str, duration: float):
-        self.name = name
-        self.author = author
-        self.duration = duration
-
+        self._Book__name = name
+        self._Book__author = author
+        self.__duration = correct_insert(duration, float)
     def __str__(self):
-        return f"Книга {self.name}. Автор {self.author}"
+        return f"Аудиокнига {self._Book__name}. Автор {self.self._Book__author}"
+
+    def get_duration(self):
+        return self.__duration
+
+    def set_duration(self, new_size: float):
+        if isinstance(new_size, float):
+            self.__duration = new_size
+        else:
+            raise ValueError("длительность аудиокниги - значение типа float")
+
+    duration = property(get_duration, set_duration)
+
+b = PaperBook("ger","Daniel",2)
+c = AudioBook("ger2","Daniel2",2.15)
+b.author = "aaa"
+c.name = "aaa"
+print(b)
+print(c.name)
